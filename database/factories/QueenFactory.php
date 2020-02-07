eed01f07f36c9e1bc956f3fd4ec0fdf81af5e24b
Dave Hoeks <dave.hoeks@gmail.com>
2020-02-07 09:53:15 +0100

    Add the queen class to the project

---
+++ database/factories/QueenFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Queen;
+use Faker\Generator as Faker;
+
+$factory->define(Queen::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ tests/Unit/QueenTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class QueenTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
