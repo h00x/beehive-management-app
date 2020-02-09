eb9ee08dfecddde4a9ec203eafdf8611ee968935
Dave Hoeks <dave.hoeks@gmail.com>
2020-02-09 16:30:44 +0100

    Add inspection model basis to the app

---
+++ database/factories/InspectionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Inspection;
+use Faker\Generator as Faker;
+
+$factory->define(Inspection::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/InspectionSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class InspectionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
