c2074ee6999d41c22bb7380624d30d05a051376f
Dave Hoeks <dave.hoeks@gmail.com>
2020-04-20 20:33:23 +0200

    Show content when model has no entries yet

--- resources/views/harvests/index.blade.php
+++ resources/views/harvests/index.blade.php
@@ -7,70 +7,80 @@
 @stop
 
 @section('content')
-    <div class="py-4 table-overview">
-        <table class="w-full">
-            <thead>
-                <tr class="text-left">
-                    <th class="py-2 px-4">Name</th>
-                    <th class="py-2 px-4">Harvest date</th>
-                    <th class="py-2 px-4">Batch code</th>
-                    <th class="py-2 px-4">Weight</th>
-                    <th class="py-2 px-4">Moister content</th>
-                    <th class="py-2 px-4">Nectar source</th>
-                    <th class="py-2 px-4">Description</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($harvests as $harvest)
-                    <tr class="hover:bg-white cursor-pointer">
-                        <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->name }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->date }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->batch_code }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->computed_weight }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->moister_content }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->nectar_source }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->description }}</td>
-
-                        <td class="flex justify-end py-2 px-4 z-20">
-                            <dropdown align="right" margin="0">
-                                <template v-slot:trigger>
-                                    <button class="text-sm text-gray-500 z-20 dots hover:text-gray-700"><i class="fas fa-ellipsis-h"></i></button>
-                                </template>
-                                <div class="hover:bg-secondary-100 -mx-2 px-2 border-b border-secondary-100">
-                                    <a href="{{ $harvest->path() . '/edit' }}" class="inline-block p-2"><i class="fas fa-edit text-sm mr-2"></i>@lang('harvests.edit')</a>
-                                </div>
-                                <div class="hover:bg-secondary-100 -mx-2 px-2 border-b border-secondary-100">
-                                    <a href="{{ $harvest->path() }}" class="inline-block p-2"><i class="fas fa-archive text-sm mr-2"></i>@lang('harvests.archive')</a>
-                                </div>
-                                <modal>
-                                    <div class="hover:bg-secondary-100 -mx-2 px-2 cursor-pointer" slot="button">
-                                        <a class="inline-block p-2 warning block text-red-800"><i class="fas fa-trash text-sm mr-2"></i>@lang('harvests.delete')</a>
-                                    </div>
-                                    <span slot="header">@lang('harvests.delete') {{ $harvest->name }}?</span>
-                                    <p slot="body">@lang('harvests.deleteBody')</p>
-                                    <span slot="cancel">@lang('general.cancel')</span>
-                                    <div slot="footer">
-                                        <a href="{{ $harvest->path() }}"
-                                           class="btn btn-warning"
-                                           onclick="event.preventDefault();
-                                               document.getElementById('delete-harvest-{{ $harvest->id }}').submit();" class="block text-red-800"
-                                        >
-                                            <i class="fas fa-trash text-sm mr-2"></i>@lang('harvests.delete')
-                                        </a>
-
-                                        <form id="delete-harvest-{{ $harvest->id }}" action="{{ $harvest->path() }}" method="POST" class="hidden">
-                                            @csrf
-                                            @method('DELETE')
-                                        </form>
-                                    </div>
-                                </modal>
-                            </dropdown>
-                        </td>
+    @if(!$harvests->count())
+        <div class="bg-white p-6 rounded-lg w-full flex justify-center items-center text-center" style="height: 75vh">
+            <div>
+                <img src="/images/bee.svg" alt="Bee free" class="w-24 m-auto">
+                <h2 class="mb-6 text-3xl">No harvests found</h2>
+                @include('layouts.button', ['text' => 'Create your first harvest', 'url' => route('harvests.create')])
+            </div>
+        </div>
+    @else
+        <div class="py-4 table-overview">
+            <table class="w-full">
+                <thead>
+                    <tr class="text-left">
+                        <th class="py-2 px-4">Name</th>
+                        <th class="py-2 px-4">Harvest date</th>
+                        <th class="py-2 px-4">Batch code</th>
+                        <th class="py-2 px-4">Weight</th>
+                        <th class="py-2 px-4">Moister content</th>
+                        <th class="py-2 px-4">Nectar source</th>
+                        <th class="py-2 px-4">Description</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{ $harvests->links() }}
-        @include('partials.errors')
-    </div>
+                </thead>
+                <tbody>
+                    @foreach ($harvests as $harvest)
+                        <tr class="hover:bg-white cursor-pointer">
+                            <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->name }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->date }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->batch_code }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->computed_weight }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->moister_content }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->nectar_source }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $harvest->path() }}';">{{ $harvest->description }}</td>
+
+                            <td class="flex justify-end py-2 px-4 z-20">
+                                <dropdown align="right" margin="0">
+                                    <template v-slot:trigger>
+                                        <button class="text-sm text-gray-500 z-20 dots hover:text-gray-700"><i class="fas fa-ellipsis-h"></i></button>
+                                    </template>
+                                    <div class="hover:bg-secondary-100 -mx-2 px-2 border-b border-secondary-100">
+                                        <a href="{{ $harvest->path() . '/edit' }}" class="inline-block p-2"><i class="fas fa-edit text-sm mr-2"></i>@lang('harvests.edit')</a>
+                                    </div>
+                                    <div class="hover:bg-secondary-100 -mx-2 px-2 border-b border-secondary-100">
+                                        <a href="{{ $harvest->path() }}" class="inline-block p-2"><i class="fas fa-archive text-sm mr-2"></i>@lang('harvests.archive')</a>
+                                    </div>
+                                    <modal>
+                                        <div class="hover:bg-secondary-100 -mx-2 px-2 cursor-pointer" slot="button">
+                                            <a class="inline-block p-2 warning block text-red-800"><i class="fas fa-trash text-sm mr-2"></i>@lang('harvests.delete')</a>
+                                        </div>
+                                        <span slot="header">@lang('harvests.delete') {{ $harvest->name }}?</span>
+                                        <p slot="body">@lang('harvests.deleteBody')</p>
+                                        <span slot="cancel">@lang('general.cancel')</span>
+                                        <div slot="footer">
+                                            <a href="{{ $harvest->path() }}"
+                                               class="btn btn-warning"
+                                               onclick="event.preventDefault();
+                                                   document.getElementById('delete-harvest-{{ $harvest->id }}').submit();" class="block text-red-800"
+                                            >
+                                                <i class="fas fa-trash text-sm mr-2"></i>@lang('harvests.delete')
+                                            </a>
+
+                                            <form id="delete-harvest-{{ $harvest->id }}" action="{{ $harvest->path() }}" method="POST" class="hidden">
+                                                @csrf
+                                                @method('DELETE')
+                                            </form>
+                                        </div>
+                                    </modal>
+                                </dropdown>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{ $harvests->links() }}
+            @include('partials.errors')
+        </div>
+    @endif
 @endsection
--- resources/views/inspections/index.blade.php
+++ resources/views/inspections/index.blade.php
@@ -7,68 +7,78 @@
 @stop
 
 @section('content')
-    <div class="py-4 table-overview">
-        <table class="w-full">
-            <thead>
-                <tr class="text-left">
-                    <th class="py-2 px-4">Hive</th>
-                    <th class="py-2 px-4">Date</th>
-                    <th class="py-2 px-4">Pollen arriving</th>
-                    <th class="py-2 px-4">Comb building</th>
-                    <th class="py-2 px-4">Weather</th>
-                    <th class="py-2 px-4">Temperature</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($inspections as $inspection)
-                    <tr class="hover:bg-white cursor-pointer">
-                        <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->hive->name }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->date }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->pollen_arriving }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->comb_building }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->weather }}</td>
-                        <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->computed_temperature }}</td>
-
-                        <td class="py-2 px-4">
-                            <dropdown align="right" margin="0">
-                                <template v-slot:trigger>
-                                    <button class="text-sm text-gray-500 z-20 dots hover:text-gray-700"><i class="fas fa-ellipsis-h"></i></button>
-                                </template>
-                                <div class="hover:bg-secondary-100 -mx-2 px-2 border-b border-secondary-100">
-                                    <a href="{{ $inspection->path() . '/edit' }}" class="inline-block p-2"><i class="fas fa-edit text-sm mr-2"></i>@lang('inspections.edit')</a>
-                                </div>
-                                <div class="hover:bg-secondary-100 -mx-2 px-2 border-b border-secondary-100">
-                                    <a href="{{ $inspection->path() }}" class="inline-block p-2"><i class="fas fa-archive text-sm mr-2"></i>@lang('inspections.archive')</a>
-                                </div>
-                                <modal>
-                                    <div class="hover:bg-secondary-100 -mx-2 px-2 cursor-pointer" slot="button">
-                                        <a class="inline-block p-2 warning block text-red-800"><i class="fas fa-trash text-sm mr-2"></i>@lang('inspections.delete')</a>
-                                    </div>
-                                    <span slot="header">@lang('inspections.delete') {{ $inspection->name }}?</span>
-                                    <p slot="body">@lang('inspections.deleteBody')</p>
-                                    <span slot="cancel">@lang('general.cancel')</span>
-                                    <div slot="footer">
-                                        <a href="{{ $inspection->path() }}"
-                                           class="btn btn-warning"
-                                           onclick="event.preventDefault();
-                                               document.getElementById('delete-inspection-{{ $inspection->id }}').submit();" class="block text-red-800"
-                                        >
-                                            <i class="fas fa-trash text-sm mr-2"></i>@lang('inspections.delete')
-                                        </a>
-
-                                        <form id="delete-inspection-{{ $inspection->id }}" action="{{ $inspection->path() }}" method="POST" class="hidden">
-                                            @csrf
-                                            @method('DELETE')
-                                        </form>
-                                    </div>
-                                </modal>
-                            </dropdown>
-                        </td>
+    @if(!$inspections->count())
+        <div class="bg-white p-6 rounded-lg w-full flex justify-center items-center text-center" style="height: 75vh">
+            <div>
+                <img src="/images/bee.svg" alt="Bee free" class="w-24 m-auto">
+                <h2 class="mb-6 text-3xl">No inspections found</h2>
+                @include('layouts.button', ['text' => 'Create your first inspection', 'url' => route('inspections.create')])
+            </div>
+        </div>
+    @else
+        <div class="py-4 table-overview">
+            <table class="w-full">
+                <thead>
+                    <tr class="text-left">
+                        <th class="py-2 px-4">Hive</th>
+                        <th class="py-2 px-4">Date</th>
+                        <th class="py-2 px-4">Pollen arriving</th>
+                        <th class="py-2 px-4">Comb building</th>
+                        <th class="py-2 px-4">Weather</th>
+                        <th class="py-2 px-4">Temperature</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{ $inspections->links() }}
-        @include('partials.errors')
-    </div>
+                </thead>
+                <tbody>
+                    @foreach ($inspections as $inspection)
+                        <tr class="hover:bg-white cursor-pointer">
+                            <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->hive->name }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->date }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->pollen_arriving }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->comb_building }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->weather }}</td>
+                            <td class="py-2 px-4" onclick="window.location='{{ $inspection->path() }}';">{{ $inspection->computed_temperature }}</td>
+
+                            <td class="py-2 px-4">
+                                <dropdown align="right" margin="0">
+                                    <template v-slot:trigger>
+                                        <button class="text-sm text-gray-500 z-20 dots hover:text-gray-700"><i class="fas fa-ellipsis-h"></i></button>
+                                    </template>
+                                    <div class="hover:bg-secondary-100 -mx-2 px-2 border-b border-secondary-100">
+                                        <a href="{{ $inspection->path() . '/edit' }}" class="inline-block p-2"><i class="fas fa-edit text-sm mr-2"></i>@lang('inspections.edit')</a>
+                                    </div>
+                                    <div class="hover:bg-secondary-100 -mx-2 px-2 border-b border-secondary-100">
+                                        <a href="{{ $inspection->path() }}" class="inline-block p-2"><i class="fas fa-archive text-sm mr-2"></i>@lang('inspections.archive')</a>
+                                    </div>
+                                    <modal>
+                                        <div class="hover:bg-secondary-100 -mx-2 px-2 cursor-pointer" slot="button">
+                                            <a class="inline-block p-2 warning block text-red-800"><i class="fas fa-trash text-sm mr-2"></i>@lang('inspections.delete')</a>
+                                        </div>
+                                        <span slot="header">@lang('inspections.delete') {{ $inspection->name }}?</span>
+                                        <p slot="body">@lang('inspections.deleteBody')</p>
+                                        <span slot="cancel">@lang('general.cancel')</span>
+                                        <div slot="footer">
+                                            <a href="{{ $inspection->path() }}"
+                                               class="btn btn-warning"
+                                               onclick="event.preventDefault();
+                                                   document.getElementById('delete-inspection-{{ $inspection->id }}').submit();" class="block text-red-800"
+                                            >
+                                                <i class="fas fa-trash text-sm mr-2"></i>@lang('inspections.delete')
+                                            </a>
+
+                                            <form id="delete-inspection-{{ $inspection->id }}" action="{{ $inspection->path() }}" method="POST" class="hidden">
+                                                @csrf
+                                                @method('DELETE')
+                                            </form>
+                                        </div>
+                                    </modal>
+                                </dropdown>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{ $inspections->links() }}
+            @include('partials.errors')
+        </div>
+    @endif
 @endsection
